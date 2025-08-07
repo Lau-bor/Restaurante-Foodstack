2a5cb89df30b05920e7ea401e3905baaa582d26f
Matthias291084 <amromano34@gmail.com>
2025-08-07 00:59:02 -0300

    Se guardan los cambios an el anlace a redes sociales del footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -8,27 +8,27 @@ function Footer() {
   <footer class="footer">
     <ul class="social-icon">
       <li class="icon-elem">
-        <a href="" class="icon">
+        <a href="https://www.youtube.com/" class="icon">
           <ion-icon name="logo-youtube"></ion-icon>
         </a>
       </li>
       <li class="icon-elem">
-        <a href="" class="icon">
+        <a href="https://www.instagram.com/" class="icon">
           <ion-icon name="logo-instagram"></ion-icon>
         </a>
       </li>
       <li class="icon-elem">
-        <a href="" class="icon">
+        <a href="https://www.whatsapp.com/" class="icon">
           <ion-icon name="logo-whatsapp"></ion-icon>
         </a>
       </li>
       <li class="icon-elem">
-        <a href="" class="icon">
+        <a href="https://www.facebook.com/" class="icon">
           <ion-icon name="logo-facebook"></ion-icon>
         </a>
       </li>
       <li class="icon-elem">
-        <a href="" class="icon">
+        <a href="https://workspace.google.com/" class="icon">
           <ion-icon name="mail-outline"></ion-icon>
         </a>
       </li>
