32da668b5bd5945fc460f4e06726c6c6f5668de4
rocio-zingaretti <rociozingaretti61@gmail.com>
2025-08-12 19:09:13 -0300

    implemente pagina 404 error con estilos

--- frontend/src/pages/NotFoundPage/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,6 +1,7 @@
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
+
 function NotFoundPage() {
 
   const navigate = useNavigate();
@@ -8,15 +9,21 @@ function NotFoundPage() {
   return (
     <div>
       <Container fluid className="vh-100 d-flex justify-content-center align-items-center bg-light">
-        <Row className="text-center">
-          <Col>
-            <h1 className="display-1 fw-bold text-success">ERROR 404</h1>
-            <h2 className="mb-3">Ups! pagina no encontrada</h2>
-            <p>La pagina que esta buscando no existe o fué removida</p>
-            <Button onClick={() => navigate('/')} variant="success" size="lg">Go Back Home</Button>
-          </Col>
-        </Row>
-      </Container>    
+  <Row className="text-center">
+    <Col>
+      <h1 
+        className="display-1 fw-bold mb-5 text-center" 
+        style={{ fontSize: '5rem', color: '#AC011F' }}>ERROR 404</h1>
+
+      <p className="text-center fs-3"> La página que estás buscando no existe o fue removida</p>
+
+      <Button onClick={() => navigate('/')} variant="success" size="lg" className='btn btn-danger btn-lg fw-bold'>
+        Volver al Inicio
+      </Button>
+    </Col>
+  </Row>
+</Container>
+
     </div>
   )
 }
