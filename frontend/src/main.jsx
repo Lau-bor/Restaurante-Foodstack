e41c9e1b12ba15200aff9f685e6e98f5069fa6ac
Son Goku <lautibor@gmail.com>
2025-08-20 12:18:28 -0300

    Realizo otros cambios para solucionar el deploy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { HashRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { AuthProvider } from './context/AuthContext.jsx';
 
-createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
       <AuthProvider>
-      <App/>
-      <Toaster position='top-center' reverseOrder={false} />
-      </AuthProvider>   
-    </BrowserRouter>,
-)
+        <App />
+        <Toaster position='top-center' reverseOrder={false} />
+      </AuthProvider>
+    </HashRouter>
+  </React.StrictMode>
+);
