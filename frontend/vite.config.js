461b7ee8b26589a717bcdcb397c9c42b148f2cb1
Son Goku <lautibor@gmail.com>
2025-08-20 11:16:17 -0300

    Más ajustes a vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-
 export default defineConfig({
   plugins: [react()],
+  base: './', 
   server: {
     proxy: {
-      '/api':{
-        target: 'http://localhost:4000',
+      '/api': {
+        target: 'http://localhost:4000', 
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false 
+        secure: false
       }
     }
   }
