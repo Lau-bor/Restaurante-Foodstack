ee781a0e3f5bc1956a7ef12c532e83bfeea96040
Son Goku <lautibor@gmail.com>
2025-08-20 15:17:22 -0300

    Modifico algunas cosas para probar el deploy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,17 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { AuthProvider } from './context/AuthContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <AuthProvider>
         <App />
         <Toaster position='top-center' reverseOrder={false} />
       </AuthProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
