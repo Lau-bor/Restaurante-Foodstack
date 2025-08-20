430d4b3134b94ff5e1898060a6cb76a45b043add
Son Goku <lautibor@gmail.com>
2025-08-20 11:55:03 -0300

    Realizo modificaciones para deployar la página

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base: './', 
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000', 
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false
-      }
-    }
+  base: './',
+  build: {
+    outDir: 'dist',
   }
 })
