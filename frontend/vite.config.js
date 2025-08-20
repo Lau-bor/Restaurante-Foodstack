0718b93ce90991d04fe1635a2cbdced05569e8fe
Son Goku <lautibor@gmail.com>
2025-08-20 12:44:09 -0300

    Realizo cambios en el vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+
 export default defineConfig({
   plugins: [react()],
-  base: './',  
+  base: './',
   build: {
     outDir: 'dist',
     sourcemap: false
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'` 
   }
-
 })
